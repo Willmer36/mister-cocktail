817f855c27ab2a1012d2f45c1b638c87c21e0575
Guillaume Grignard <a1z2e3r4t5y67890@gmail.com>
2016-02-18 17:55:12 +0100

    a finir

--- app/models/cocktail.rb
+++ app/models/cocktail.rb
@@ -1,5 +1,5 @@
 class Cocktail < ActiveRecord::Base
   validates :name, uniqueness: true, presence: true
-  has_many :doses
+  has_many :doses, dependent: :destroy
   has_many :ingredients, through: :doses
 end
